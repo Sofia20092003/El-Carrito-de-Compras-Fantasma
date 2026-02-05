695737e76e5acc00df4de9d1fb5a94d7c5eb0970
Sofia20092003 <angelasofiarodriguezarcinegas03@liceolapaz.net>
2026-02-05 12:57:52 +0100

    Clase Main Terminada

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,15 @@
 public class Main {
     public static void main(String[] args) {
 
+        Carrito carritoJuan= new Carrito("Juan");
+        Carrito carritoMaria= new Carrito("Maria");
+
+        carritoJuan.agregarProducto(new Producto("Manzana", 1.0));
+        carritoMaria.agregarProducto(new Producto("Pera", 1.2));
+
+        System.out.println("Productos de Juan: ");
+        for (Producto p: Carrito.listaProductos){
+            System.out.println(p);
+        }
     }
 }
