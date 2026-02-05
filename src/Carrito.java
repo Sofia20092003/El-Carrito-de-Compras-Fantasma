c6984cf4fcfec7c280c5ec9cb0ac1d009ca75f0a
Sofia20092003 <angelasofiarodriguezarcinegas03@liceolapaz.net>
2026-02-05 12:53:41 +0100

    Clase Carrito Terminada

--- src/Carrito.java
+++ src/Carrito.java
@@ -1,2 +1,17 @@
+import java.util.ArrayList;
+
 public class Carrito {
+
+    /*Error intencionado*/
+    public static ArrayList<Producto> listaProductos= new ArrayList<>();
+
+    private String cliente;
+
+    public Carrito(String cliente) {
+        this.cliente = cliente;
+    }
+
+    public void agregarProducto(Producto p){
+        listaProductos.add(p);
+    }
 }
